1d8dfdbb1f4ffc60727b4965f1bac27a92e1fb46
lafont-loic <loic.lafont.isr@gmail.com>
2026-01-29 10:32:48 +0100

    Update sizeof_types.c

--- TP1/src/sizeof_types.c
+++ TP1/src/sizeof_types.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
+#include <string.h>
 
+int main() {
+printf(sizeof.char)
+printf(sizeof.short)
+printf(sizeof.int)
+printf(sizeof.long int)
+printf(sizeof.float)
+printf(sizeof.double)
+printf(sizeof.long double)
+}
