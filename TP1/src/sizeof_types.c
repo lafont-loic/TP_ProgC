4d048a3bdeeef4594d6f7b154eaf4e8322ee8eba
lafont-loic <loic.lafont.isr@gmail.com>
2026-01-29 11:39:59 +0100

    Update sizeof_types.c

--- TP1/src/sizeof_types.c
+++ TP1/src/sizeof_types.c
@@ -3,24 +3,25 @@
 
 int main() {
 printf("La taille en octect pour char signed est %d \n" , sizeof(char));
-printf("La taille en octect pour char unsigned est %d \n" , sizeof(char));
+printf("La taille en octect pour char unsigned est %d \n" , sizeof(unsigned char));
 
   
 printf("La taille en octect pour short signed est %d \n" , sizeof(short));
-printf("La taille en octect pour short unsigned est %d \n" , sizeof(short));
+printf("La taille en octect pour short unsigned est %d \n" , sizeof(unsigned short));
   
 printf("La taille en octect pour int signed est %d \n" , sizeof(int));
-printf("La taille en octect pour int unsigned est %d \n" , sizeof(int));
+printf("La taille en octect pour int unsigned est %d \n" , sizeof(unsigned int));
 
   
 printf("La taille en octect pour long int signed est %d \n" , sizeof(long int));
-printf("La taille en octect pour long int unsigned est %d \n" , sizeof(long int));
+printf("La taille en octect pour long int unsigned est %d \n" , sizeof(unsigned long int));
 
   
 printf("La taille en octect pour float est %d \n" , sizeof(float));
 printf("La taille en octect pour double est %d \n" , sizeof(double));
 printf("La taille en octect pour long double est %d \n" , sizeof(long double));
 }
+
 
 
 
