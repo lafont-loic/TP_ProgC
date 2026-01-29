013961a377296970bd534944370c2edee8101001
lafont-loic <loic.lafont.isr@gmail.com>
2026-01-29 12:46:38 +0100

    Update opérateurs.c

--- TP1/src/opérateurs.c
+++ TP1/src/opérateurs.c
@@ -5,10 +5,13 @@ int main() {
 int a = 5;
 int b = 6;
 int resultat = a + b;
+int resultatsous = a - b;
 int somme = a * b;
 printf( "le résultat du calcul est %d \n" , resultat);
-printf( "le résultat du calcul est %d \n" , somme);
+printf( " le résultat de la soustraction est %d \n" , resultatsous);
+printf( "la somme du calcul est %d \n" , somme);
 
 }
+
 
 
