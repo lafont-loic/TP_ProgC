36419b0b0c3f324bc13893d85e381e8586ce8579
lafont-loic <loic.lafont.isr@gmail.com>
2026-01-29 12:30:02 +0100

    Update variables.c

--- TP1/src/variables.c
+++ TP1/src/variables.c
@@ -6,10 +6,15 @@ int main() {
     
     // Je crée une boîte et je mets ma valeur dedans dedans
     char ma_lettre = 'A';
+    unsigned char ma_lettre = 'B';
     int mon_nombreint = 50;
+    unsigned int mon_nombreint = 50;
     short mon_nombreshort = -10;
+    unsigned short mon_nombreshort = -10;
     long int mon_nombrelongint = 200000000;
+    unsigned long int mon_nombrelongint = 80000000;
     long long int mon_nombrelonglong = 9999999999999999;
+    unsigned long long int mon_nombrelonglong = 77777777777777777;
     float mon_nombrefloat = 19.5f;
     double mon_nombredouble = 3.1415926535;
     long double mon_nombrelongdouble = 3.14159265358979323846L;
@@ -18,14 +23,20 @@ int main() {
 
     // --- ÉTAPE 2 : AFFICHER LE CONTENU ---
     printf("Le contenu de ma variable char est : %c \n", ma_lettre);
+    printf("Le contenu de ma variable unsigned char est : %d \n", ma_lettre);
     printf("Le contenu de ma variable int est : %d \n", mon_nombreint);
+    printf("Le contenu de ma variable unsigned int est : %u \n", mon_nombreint);
     printf("Le contenu de ma variable short est : %hd \n", mon_nombreshort);
+    printf("Le contenu de ma variable unsigned short est : %hu \n", mon_nombreshort);
     printf("Le contenu de ma variable long int est : %ld \n", mon_nombrelongint);
+    printf("Le contenu de ma variable unsigned long int est : %lu \n", mon_nombrelongint);
     printf("Le contenu de ma variable long long int est : %lld \n", mon_nombrelonglong);
+    printf("Le contenu de ma variable unsigned long long int est : %llu \n", mon_nombrelonglong);
     printf("Le contenu de ma variable float est : %f \n", mon_nombrefloat);
     printf("Le contenu de ma variable doucbe est : %lf \n", mon_nombredouble);
-    printf("Le contenu de ma variable double est : %Lf \n", mon_nombrelongdouble);
+    printf("Le contenu de ma variable long double est : %Lf \n", mon_nombrelongdouble);
 
     return 0;
 }
+
 
