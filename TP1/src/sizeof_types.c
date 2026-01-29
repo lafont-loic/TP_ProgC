7d36936fec73b8e76b013a399bab65a58d242b16
lafont-loic <loic.lafont.isr@gmail.com>
2026-01-29 11:35:49 +0100

    Update sizeof_types.c

--- TP1/src/sizeof_types.c
+++ TP1/src/sizeof_types.c
@@ -3,13 +3,25 @@
 
 int main() {
 printf("La taille en octect pour char est %d \n" , sizeof(char));
+printf("La taille en octect pour char est %d \n" , sizeof(char));
+
+  
 printf("La taille en octect pour short est %d \n" , sizeof(short));
+printf("La taille en octect pour short est %d \n" , sizeof(short));
+  
 printf("La taille en octect pour int est %d \n" , sizeof(int));
+printf("La taille en octect pour int est %d \n" , sizeof(int));
+
+  
 printf("La taille en octect pour long int est %d \n" , sizeof(long int));
+printf("La taille en octect pour long int est %d \n" , sizeof(long int));
+
+  
 printf("La taille en octect pour float est %d \n" , sizeof(float));
 printf("La taille en octect pour double est %d \n" , sizeof(double));
 printf("La taille en octect pour long double est %d \n" , sizeof(long double));
 }
+
 
 
 
