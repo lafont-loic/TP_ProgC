c3430cb10389224f658e8bbda7ac8889fb01803a
lafont-loic <loic.lafont.isr@gmail.com>
2026-01-29 12:43:51 +0100

    Update opérateurs.c

--- TP1/src/opérateurs.c
+++ TP1/src/opérateurs.c
@@ -5,6 +5,10 @@ int main() {
 int a = 5;
 int b = 6;
 int resultat = a + b;
+int somme = a * b;
 printf( "le résultat du calcul est %d \n" , resultat);
+printf( "le résultat du calcul est %d \n" , somme);
+
 }
+
 
