35dd7e521b8fdd32ab4f9bb339d2bc97f9ce6b68
lafont-loic <loic.lafont.isr@gmail.com>
2026-01-29 10:44:27 +0100

    Update sizeof_types.c

--- TP1/src/sizeof_types.c
+++ TP1/src/sizeof_types.c
@@ -2,12 +2,13 @@
 #include <string.h>
 
 int main() {
-printf(sizeof(char));
-printf(sizeof(short));
-printf(sizeof(int));
-printf(sizeof(long int));
-printf(sizeof(float));
-printf(sizeof(double));
-printf(sizeof(long double));
+printf("" , sizeof(char));
+printf("" , sizeof(short));
+printf("" , sizeof(int));
+printf("" , sizeof(long int));
+printf("" , sizeof(float));
+printf("" , sizeof(double));
+printf("" , sizeof(long double));
 }
+
 
